112586456cd700b2fb8e95ba0fdee091f21953b9
daveKidd <david.kidd@lambdaschool.com>
2021-02-10 11:09:37 -0700

    ketchup

--- api/server.js
+++ api/server.js
@@ -6,8 +6,31 @@ const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
+
+const logQuote = (coin) => (req,res,next) =>{
+  if(coin === "dime" || coin ==="penny" || coin === "nickel" || coin ==="quarter"){
+    console.log(`A ${coin} saved is a ${coin} not enjoyed (:`)
+    next()
+  }else{
+    res.json("not a valid coin")
+  }
+}
+
+const checkWord = (req,res,next) =>{
+  
+}
+
+
+
+// function logQuote(req,res,next){
+//   console.log("A penny saved is a penny not enjoyed (:")
+//   next()
+// }
+
+
 server.use(helmet())
 server.use(express.json(), morgan("dev"));
+server.use(logQuote("whatever"))
 //server.use(morgan("dev"))
 
 server.use('/api/hubs', hubsRouter);
