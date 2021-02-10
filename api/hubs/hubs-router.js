8c7df7572105ff54563412f534d554f79af62b39
daveKidd <david.kidd@lambdaschool.com>
2021-02-10 11:44:53 -0700

    ketchup

--- api/hubs/hubs-router.js
+++ api/hubs/hubs-router.js
@@ -5,6 +5,18 @@ const Messages = require('../messages/messages-model.js');
 
 const router = express.Router();
 
+const checkHubId = async (req,res,next)=>{
+  const {id} = req.params
+  const hub = await Hubs.findById(id)
+  if(!hub){
+    res.status(400).json({message: `No hub with id: ${id}`})
+  }else{
+    req.hub = hub
+    next()
+  }
+}
+
+
 router.get('/', (req, res) => {
   Hubs.find(req.query)
     .then(hubs => {
@@ -19,22 +31,8 @@ router.get('/', (req, res) => {
     });
 });
 
-router.get('/:id', (req, res) => {
-  Hubs.findById(req.params.id)
-    .then(hub => {
-      if (hub) {
-        res.status(200).json(hub);
-      } else {
-        res.status(404).json({ message: 'Hub not found' });
-      }
-    })
-    .catch(error => {
-      // log error to server
-      console.log(error);
-      res.status(500).json({
-        message: 'Error retrieving the hub',
-      });
-    });
+router.get('/:id',checkHubId, (req, res) => {
+  res.status(200).json(req.hub)
 });
 
 router.post('/', (req, res) => {
@@ -51,7 +49,7 @@ router.post('/', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', checkHubId, (req, res) => {
   Hubs.remove(req.params.id)
     .then(count => {
       if (count > 0) {
@@ -69,7 +67,7 @@ router.delete('/:id', (req, res) => {
     });
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', checkHubId, (req, res) => {
   Hubs.update(req.params.id, req.body)
     .then(hub => {
       if (hub) {
@@ -87,7 +85,7 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.get('/:id/messages', (req, res) => {
+router.get('/:id/messages',checkHubId, (req, res) => {
   Hubs.findHubMessages(req.params.id)
     .then(messages => {
       res.status(200).json(messages);
@@ -101,7 +99,7 @@ router.get('/:id/messages', (req, res) => {
     });
 });
 
-router.post('/:id/messages', (req, res) => {
+router.post('/:id/messages',checkHubId, (req, res) => {
   const messageInfo = { ...req.body, hub_id: req.params.id };
 
   Messages.add(messageInfo)
